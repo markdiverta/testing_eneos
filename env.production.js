ff4c9a23ac8711a5582a8650fa5e322a12e24006
Wayne Koay <wayne@diverta.co.jp>
2023-05-25 16:11:04 +0900

    Revert base URL changes

--- env.production.js
+++ env.production.js
@@ -3,5 +3,5 @@ module.exports = {
     SITE_TITLE:'MTown - マレーシアの週刊情報誌MtownのWEBサイト。マレーシアライフをもっと楽しく豊かに、をコンセプトに、在留邦人や短期滞在者、またマレーシアに興味がある方に、実用的で生活に役立つ情報を提供しています。',
     SITE_DESCRIPTION:'マレーシアの週刊情報誌MtownのWEBサイト。マレーシアライフをもっと楽しく豊かに、をコンセプトに、在留邦人や短期滞在者、またマレーシアに興味がある方に、実用的で生活に役立つ情報を提供しています。',
     ROBOTS: 'index',
-    BASE_URL: 'https://www.mtown.my/'
+    BASE_URL: 'https://api.mtown.my'
 };
